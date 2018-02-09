c69a8f3c8c31117c06ecb0007e4fccd2cd4b007c
Ray Suyoto <ray.suy@icloud.com>
2018-02-09 11:50:42 -0700

    colorize gem

---
+++ git_practice/main.rb
@@ -0,0 +1,22 @@
+require colorize
+def puts_git(cmd)
+    puts `git #{cmd} -h`
+  end
+  
+  def menu
+    puts '1: Enter git command'.colorize(:cyan)
+    puts '2: Exit'
+    choice = gets.to_i
+    case choice
+      when 1
+        puts 'Enter git command'
+        puts_git(gets.strip)
+        menu
+      when 2
+        exit
+      else
+        puts 'Invalid choice'
+        menu
+    end
+  end
+menu
